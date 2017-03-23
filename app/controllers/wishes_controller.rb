4030bc60e116f95aa36dccdaae9cfd3e22679aaf
Takach1 <tak103.fox7@gmail.com>
2017-03-23 22:57:48 +0900

    newリンクの追加

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,28 +1,28 @@
 class WishesController < ApplicationController
     def index
-        @wish = Wish.all
+      @wish = Wish.all
     end
+
     def new
       @wish =Wish.new
     end
+
     def create
-def create
-  @wish = Wish.new(wish_params)
-  if @wish.save
+      @wish = Wish.new(wish_params)
+        if @wish.save
     # @userはuser_path(@user) に自動変換される
-    redirect_to @wish, notice: "追加しました。"
-  else
+        redirect_to '/wishes', notice: "追加しました。"
+        else
     # ValidationエラーなどでDBに保存できない場合 new.html.erb を再表示
-    render 'new'
-  end
+        render 'new'
+        end
+    end
 
   private
     # Rails4からStrongParamaterと呼ばれる機能が追加されました。
     # セキュリティのため、permitメソッドで許可したパラメータ名しか取得できません。
     def wish_params
       params.require(:wish).permit(:item)
-    end
-end
+  end
 
-    end
 end
